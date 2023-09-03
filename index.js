a8ba90240d76bf784157019043bdd80eee5d30c0
Alezito2008 <92759854+Alezito2008@users.noreply.github.com>
2023-09-03 01:27:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+var teclas = ''
+var lastKeys = []
+var prediction = ''
+
+var persona = 0
+var maquina = 0
+
+var maxPoints = 5
+var gameOver = false
+
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random()*this.length))];
+}
+
+// Detectar teclas
+document.addEventListener('keyup', (e) => {
+    if (!gameOver) {
+        switch (e.key) {
+            case 'ArrowRight':
+                handlePress('R')
+                break
+            case 'ArrowLeft':
+                handlePress('L')
+                break
+        }
+    }
+})
+
+function handlePress(key) {
+    // Sumar los puntos
+    prediction === key ? maquina++ : persona++
+    updatePoints()
+
+    // Detectar ganador
+    persona === maxPoints || maquina === maxPoints ? gameOver = true : void(0)
+
+    teclas += key
+    lastKeys.push(key)
+
+    if (lastKeys.length >= 4) {
+        if (findNext(lastKeys) != null) {
+            prediction = findNext(lastKeys)
+        }
+        lastKeys.shift()
+    }
+
+    if (gameOver) {
+        document.querySelector('div.puntaje').classList.add('ganador')
+        document.querySelector('div.puntaje>button').setAttribute('style', '')
+        if (persona === maxPoints) {
+            document.querySelector('div.puntaje>h2').textContent = 'Ganó persona'
+        } else {
+            document.querySelector('div.puntaje>h2').textContent = 'Ganó máquina'
+        }
+    }
+}
+
+function findNext(l) {
+    let lastKeysStr = l.toString().replaceAll(',', '')
+    if (teclas.indexOf(lastKeysStr != -1)) {
+        let pos = teclas.indexOf(lastKeysStr)+4
+        if (pos != teclas.length) {
+            return teclas[pos]
+        }
+    } else {
+        return ['L', 'R'].random()
+    }
+}
+
+function updatePoints() {
+    document.querySelector('div.persona').textContent = `Persona: ${persona}`
+    document.querySelector('div.maquina').textContent = `Máquina: ${maquina}`
+}
